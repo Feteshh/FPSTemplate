c929da7705aee6fe02b6f662d02d39f89e564e4a
Darcy <104533465+Feteshh@users.noreply.github.com>
2026-03-16 11:14:18 +0000

    BP_Item Practical

---
+++ Source/FPSTemplate/Item.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+
+#include "Components/TextBlock.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Model = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Item Model"));
+	RootComponent = Model;
+	Text = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Item Title"));
+	Text->SetupAttachment(Model);
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Item Collider"));
+	Collider->SetupAttachment(Model);
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	UpdateTitle();
+	
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UWorld* world = GetWorld();
+	if (world)
+	{
+		AController* controller = world->GetFirstPlayerController();
+		if (controller)
+		{
+			AActor* playerPawn = controller->GetPawn();
+			
+			if (playerPawn)
+			{
+				FRotator lookAt = UKismetMathLibrary::FindLookAtRotation(
+					GetActorLocation(),
+					playerPawn->GetActorLocation());
+				Text->SetWorldRotation(lookAt);
+			}
+		}
+	}
+
+}
+
+void AItem::UpdateTitle()
+{
+	if (Text)
+	{
+		Text->SetText(FText::FromString(itemDetail.ItemName));
+		Text->SetTextRenderColor(RarityColor[itemDetail.Rarity]);
+	}
+}
+
+
---
+++ Source/FPSTemplate/Item.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "Components/TextRenderComponent.h"
+#include "Item.generated.h"
+
+UENUM(BlueprintType)
+enum class ERarity: uint8
+{
+	Common UMETA(DisplayName = "Common"),
+	UnCommon UMETA(DisplayName = "UnCommon"),
+	Rare UMETA(DisplayName = "Rare"),
+	Epic UMETA(DisplayName = "Epic"),
+	Legendary UMETA(DisplayName = "Legendary")
+};
+
+USTRUCT(BlueprintType)
+struct FItemData
+{
+	GENERATED_USTRUCT_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString ItemName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ERarity Rarity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BaseValue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Weight;
+};
+
+UCLASS()
+class FPSTEMPLATE_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void UpdateTitle();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FItemData itemDetail;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TMap<ERarity,FColor> RarityColor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Model;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTextRenderComponent* Text;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UBoxComponent* Collider;
+	
+	
+};
